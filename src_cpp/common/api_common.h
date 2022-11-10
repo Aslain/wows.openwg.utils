3380d53ccbf011f1ff78a7fff6055af72d4f7745
Mikhail Paulyshka <me@mixaill.tk>
2022-11-10 22:09:56 +0300

    common: fix build

--- src_cpp/common/api_common.h
+++ src_cpp/common/api_common.h
@@ -7,10 +7,20 @@
 extern "C" {
 #endif
 
+#include <stdint.h>
+
 #ifdef BUILD_OPENWGUTILS
-    #define XVMEXT_API_CALL __declspec(dllexport)
+#define XVMEXT_API_CALL __declspec(dllexport)
 #else
-    #define XVMEXT_API_CALL __declspec(dllimport)
+#define XVMEXT_API_CALL __declspec(dllimport)
+#endif
+
+#ifndef _In_
+#define _In_
+#endif
+
+#ifndef _Out_
+#define _Out_
 #endif
 
 #ifdef __cplusplus
