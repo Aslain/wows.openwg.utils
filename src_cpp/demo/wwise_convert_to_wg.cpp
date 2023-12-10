f513275ddd4d34825ee12d38168874ed98690581
Mikhail Paulyshka <me@mixaill.net>
2023-12-10 18:32:54 +0300

    v2023.09.21: add console utils for wwise relicensing

---
+++ src_cpp/demo/wwise_convert_to_wg.cpp
@@ -0,0 +1,66 @@
+//
+// Includes
+//
+
+// stdlib
+#include <filesystem>
+#include <iostream>
+
+// openwg
+#include "wwise/api_wwise.h"
+
+
+//
+// App
+//
+
+int main(int argc, char* argv[]) {
+    std::cout << "OpenWG.Utils: WWISE license converter" << std::endl;
+    std::cout << "Converting " << argc - 1 << " files" << std::endl;
+    for (size_t idx = 1; idx < argc; idx++) {
+        std::filesystem::path filepath(argv[idx]);
+        std::cout << " * " << filepath << std::endl;
+        if (!std::filesystem::exists(filepath)) {
+            std::cout << "    - FAIL, path does not exists" << std::endl;
+            continue;
+        }
+        if (!std::filesystem::is_regular_file(filepath)) {
+            std::cout << "    - FAIL, path is not a file" << std::endl;
+            continue;
+        }
+
+        auto *wwise = WWISE_OpenFileW(filepath.wstring().c_str());
+        if (!wwise) {
+            std::cout << "    - FAIL, failed to open file" << std::endl;
+            continue;
+        }
+
+        auto license = WWISE_LicenseGet(wwise);
+        if (license == WwiseLicense_Wargaming) {
+            std::cout << "    - SKIP, license is equal to Wargaming.net" << std::endl;
+            WWISE_Close(wwise);
+            continue;
+        }
+        if (license != WwiseLicense_Unlicensed) {
+            std::cout << "    - FAIL, cannot detect the license" << std::endl;
+            WWISE_Close(wwise);
+            continue;
+        }
+        if (!WWISE_LicenseSet(wwise, WwiseLicense_Wargaming)) {
+            std::cout << "    - FAIL, cannot change the license" << std::endl;
+            WWISE_Close(wwise);
+            continue;
+        }
+        if (!WWISE_SaveFileW(wwise, filepath.wstring().c_str())) {
+            std::cout << "    - FAIL, cannot save the changes" << std::endl;
+            WWISE_Close(wwise);
+            continue;
+        }
+
+        std::cout << "    - OK" << std::endl;
+        WWISE_Close(wwise);
+    }
+
+    std::cin.get();
+    return 0;
+}
--- src_innosetup_demo/openwg_utils_demo.iss
+++ src_innosetup_demo/openwg_utils_demo.iss
@@ -1,4 +1,4 @@
-﻿// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT
 // Copyright (c) 2017-2022 OpenWG.Utils Contributors
 
 #define APP_WEBSITE    "https://gitlab.com/openwg/openwg.utils"
