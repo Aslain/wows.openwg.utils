bd954c4edc07cbbca4b00039971f4bc4ef6e6b2a
Mikhail Paulyshka <me@mixaill.net>
2024-12-10 03:54:08 +0300

    cmake: update dependencies

--- src/_cmake/cpm-catch2.cmake
+++ src/_cmake/cpm-catch2.cmake
@@ -1,5 +1,5 @@
 CPMAddPackage(
   NAME Catch2
   GITHUB_REPOSITORY catchorg/Catch2
-  VERSION 3.4.0
+  VERSION 3.7.1
 )
--- src/_cmake/cpm-stb.cmake
+++ src/_cmake/cpm-stb.cmake
@@ -1,11 +1,15 @@
 CPMAddPackage(
   NAME stb
   GITHUB_REPOSITORY nothings/stb
-  GIT_TAG 5736b15f7ea0ffb08dd38af21067c314d6a3aae9
+  GIT_TAG 5c205738c191bcb0abc65c4febfa9bd25ff35234
+  VERSION 2024.11.09
   DOWNLOAD_ONLY True
 )
 
 if(stb_ADDED)
   add_library(stb INTERFACE)
   target_include_directories(stb INTERFACE "${stb_SOURCE_DIR}")
+  if(WIN32)
+    target_compile_definitions(stb INTERFACE "STBI_WINDOWS_UTF8")
+  endif()
 endif()
